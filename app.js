d3fe3f5a3b8370f6a30cc9beadcb5c8d895471cb
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-02-07 11:26:00 +0530

    solved maths

---
+++ app.js
@@ -0,0 +1,18 @@
+let val;
+
+val=Math.PI;
+val=Math.E
+val=Math.round(2.4)
+val=Math.ceil(2.4)
+val=Math.floor(2.8)
+val=Math.sqrt(64)
+val=Math.abs(-3)
+val=Math.pow(8,2)
+val=Math.min(2,33,-5,6,10)
+val=Math.max(2,33,-5,6,10)
+
+val=Math.random() 
+val=Math.random() *20
+val=Math.random() *20 + 1
+val=Math.floor(Math.random() *20 + 1)
+console.log(val)
