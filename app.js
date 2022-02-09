ca0f246e08cee3300edcba59f4bb4356a0ff1415
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-02-09 23:56:32 +0530

    loops done

--- app.js
+++ app.js
@@ -1,65 +1,23 @@
-//Function declaration
-function greet(){
-    console.log('hello');
-}
-greet(); // hello
+//Loop though array
+const cars=['ford','tata','mahindra','honda'];
 
-function greet(){
-    return 'hello';
-}
-greet();//  not give any output in console
-console.log(greet()); // hello
-
-//Passing Arguments
-function greet(firstname, lastname){
-    return 'hello ' + firstname + ' '+  lastname ;
+for(let i=0; i<cars.length; i++){
+    console.log(cars[i]);
 }
 
-console.log(greet ('hardik','sinha'))  // hello hardik sinha
+cars.forEach(function(car , index, array){
+    console.log(`${index} : ${car}`);
+})
 
-// passing default values
-function greet(firstname="hardik",lastname='name'){
-    return 'hello ' + firstname + ' '+  lastname ;
-}
+const users =[
+    {id:1 , name:'john'},
+    {id:2 , name:'jack'},
+    {id:3 , name:'mickry'},
+    {id:4 , name:'sam'}
+]
 
-console.log(greet()); // hello hardik sinha  here we are not passing any parameter but in function only we set the default values
+const ids=users.map(function(user){
+    return user.id;
+})
 
-
-//Function Expressions
-const square =function(x){
-    return x*x;
-
-}
-
-console.log(square(3)) // 9
-
-//IMMIDIATELY INVOKABLE FUNCTION EXPRESSION - IIFEs
-//The function that declare and run at the same time.
-
-// (function(){
-//     console.log('IIFE Ran..');
-//   })();
-
-// (function(name){
-//     console.log('hello ' + name);
-// })('hardik');
-
-//Property Methods
-
-const todo ={
-    add: function(){
-        console.log('add todo')
-    },
-    edit: function(id){
-        console.log('edit done ' + id)
-    }
-
-}
-
-todo.delete =function(){
-    console.log('delete todo')
-}
-
-todo.add(); // add todo
-todo.edit(3); //edit done 3
-todo.delete(); // delete todo
+console.log(ids)
