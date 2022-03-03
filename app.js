3c0d9e92a1e36ceacaf6f7c36fa40d2d16f82146
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-03-03 21:00:04 +0530

    eventlistener

--- app.js
+++ app.js
@@ -1,46 +1,41 @@
+document.querySelector('.clear-tasks').addEventListener('click',function(e){
+    console.log('hello world');
+
+    
+})
+
+document.querySelector('.clear-tasks').addEventListener('click',onclick);
+
+// e is just an event nothing else
+function onclick(e){
+    // console.log('clicked');
+
+    let val;
+
+    val = e;
+
+    
+    console.log(val); // PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
+
+    // Event target element
+    val = e.target; // it will give that actual element which we are targeting. here we are targeting button (clear tasks)
+    val = e.target.id;// it will give id associated with it, since here no id so no result
+    val = e.target.className; // clear-tasks btn black, these are classes associated with it
 
 
-//Creating Elements
-const newHeading=document.createElement('h2');
-//Add id
-newHeading.id= 'task-title';
-//new text node
-newHeading.appendChild(document.createTextNode('Task List'));// <h2 id="task-title">Task List </h2>
+    //Event type
+    val= e.type; // click, since it has type click if it is written there mouseover then it would have shown mouseover
 
-//get the old heading
-const oldHeading =document.getElementById('task-title');
-//Parent (we select this becoz we need to do operations on child)
-const cardAction = document.querySelector('.card-action');
+    //Timestamp
+    val = e.timeStamp;// it will give time
 
-//Replace Element
-cardAction.replaceChild(newHeading , oldHeading); 
+    //Coordinate event relative to the window
+    val= e.clientY;
+    val= e.clientX;
 
-//Remove Element
-const lis= document.querySelectorAll('li');
-const list = document.querySelector('ul')
+    //Coordinate event relative to the element
+    val= e.offsetY;
+    val= e.offsetX;
 
-//Remove list item
-lis[0].remove(); //it will remove the index 0 list item
-
-//Remove child ELement
-list.removeChild(lis[3])
-
-
-//Classes
-const firstLi =document.querySelector('li:first-child');
-
-console.log(firstLi.children[0]);
-
-val = link.className;
-val = link.classList;
-val = link.classList[0];
-link.classList.add('test'); // adding class
-link.classList.remove('test');
-
-
-
-//Attributes
-val = link.getAttribute('href');
-val = link.setAttribute('href','http://google.com')
-
-console.log(val);
+    console.log(val);//
+}
