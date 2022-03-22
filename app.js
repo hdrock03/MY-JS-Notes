4349d6add464a47bdddd62c5d4a97c7c3d58dd79
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-03-22 14:15:54 +0530

    prototype done

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-// const brad = {
-//     name: 'Brad',
-//     age: 30
-// }
-
-// console.log(brad); // {name: 'Brad', age: 30}
-
-// console.log(brad.age); // 30
-
-// Person Constructor
-// costructor should start with capital letter
-// function Person() { 
-//     this.name = 'Hardik';
-
-// }
-
-// const hardik = new Person(); // Person {name: 'Hardik'}
-// console.log(hardik); 
-// const sinha = new Person();  // Person {name: 'Hardik'} becoz in constructor we used this.name = Hardik
-// console.log(sinha); 
-
-
-
-//so we need to take argument and then watever we use we will get different names
-
-function Person(name,age) { 
-    this.name = name;
-    this.age = age;
-
+//People Constructor
+function Person(firstName , lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
 }
 
-const hardik = new Person('Hardik', 25 ); 
+// Greeting
+Person.prototype.greeting = function(){
+    return `Hello there ${this.firstName} ${this.lastName}`;
+}
 
-const sinha = new Person('Sinha' , 32); // Person {name: 'Sinha', age: 32}
-console.log(sinha); 
+const person1 = new Person('John' , 'Doe');
+
+console.log(person1.greeting()); // Hello there John Doe
+
+//Customer Constructor
+ function Customer(firstName, lastName, phone, membership){
+    Person.call(this,firstName,lastName); //Person.call se uska property le liye jaise firstname and lastname and this nh rhega to aayega nh dono chiz
+    
+    this.phone = phone;
+    this.membership = membership;
+ }
+ // Inherit the person prototype methods
+
+ Customer.prototype = Object.create(Person.prototype)
+
+ //create customer
+ const customer1 = new Customer('tom','smith','55555555','standard');
+
+ console.log(customer1);// Customer {firstName: 'tom', lastName: 'smith', phone: '55555555', membership: 'standard'}
+
+//  console.log(customer1.greeting()) ;//Uncaught TypeError: customer1.greeting is not a function, here it doesnt inherit the property
+
+
+ console.log(customer1.greeting());// Hello there tom smith
