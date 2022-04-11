7fab6b0d268eecaa2a9f4e1bcf4fdb3064416318
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-04-11 15:39:44 +0530

    ajax

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-class Person{
-    constructor(firstName, lastName, dob){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthday = new Date (dob);
+ document.getElementById('button').addEventListener('click',loadData);
+
+function loadData(){
+    console.log('1')
+    //Create an XHR object
+    const xhr = new XMLHttpRequest();
+
+    // so this has some properties
+    //OPEN - iT IS SOMENTHING WHERE WE CAN SPECIFY THE TYPE OF REQUEST OR THE URL OR FILE NAME WE WANT TO MAKE IT TO
+    // SYNTAX- varname('the type of req we are gonna get to the file', 'file', for asynchronous it must be true)
+    xhr.open('GET', 'data.txt', true);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            console.log(this.responseText) //1 app.js:15 Some plain text Data
+            
+        }
+
     }
 
-    greeting(){
-        return `Hello there ${this.firstName} ${this.lastName}`
-    }
+    xhr.send();
 
-    calculateAge(){
-        const diff = date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970)
-    }
+    //HTTP Status
+    // 200: "OK"
+    // 403: "fORBIDDEN"
+    // 404: "Not found"
 
-    getsMarried(newLastName){
-        this.lastName = newLastName;
-    }
+    //readystate values 
+    // 0 : request not initiaized
+    // 1 : server connection established
+    // 2 : request received
+    // 3 : processing request 
+    // 4 : request finished and response is ready
 }
 
-const mary = new Person('Mary','Williams','11-13-1998');
-
-mary.getsMarried('Thompson')// Person {firstName: 'Mary', lastName: 'Thompson', birthday: Fri Nov 13 1998 00:00:00 GMT+0530 (India Standard Time)}
-
-console.log(mary)
