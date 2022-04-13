387840e9b855f103de06d77aaa9eab868f73fd89
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-04-13 16:26:34 +0530

    fetch done

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
-// NORMAL FUNCTION
-// const sayHello = function(){
-//     console.log('hello')
-// }
+document.getElementById('button1').addEventListener('click',getText);
+document.getElementById('button2').addEventListener('click',getJson);
+document.getElementById('button3').addEventListener('click',getUrl);
 
-//ARROW FUNCTION
-// const sayHello = () =>{
-//     console.log('hello')
-// }
+//Get local text data
+function getText(){
+    fetch('data.txt')
+    .then(res => res.text())
+    .then(data =>{
+        console.log(data); // Some plain text Data
+		document.querySelector(".output").innerHTML= data; // it will print output on screen
+        
+    })
+    .catch(err => console.log(err))
+}
 
-//One line function does not need braces
-// const sayHello = () => console.log('hello')
+// Get local Json Data
+function getJson(){
+    fetch('customer.json')
+    .then(res => res.json())
 
-//One line returns
-// const sayHello = () => 'hello';
+    .then(data => {
+            console.log(data);// 
+         let output = '';
+         data.forEach(function(post){
+             output += `<li>${post.title}</li>`;
 
-//Same as above
-// const sayHello = function() {
-//     return 'hello';
-// } 
+         });
+         document.querySelector(".output").innerHTML= output; // it will print on screen
+        })
+        .catch(err => console.log(err));
+    }
 
-//Return Object
-// const sayHello = () => ({msg: 'hello'})//{msg: 'hello'}
-
-// const sayHello = (name) => console.log(`hello ${name}`)
-
-// sayHello('hardik')
-
-
-// const sayHello = (name,age) => console.log(`hello ${name} ${age}`)
-// sayHello('hardik', 24)
-
-//MAP FUNCTION
-const users =['Nathan', 'Mac','John'];
-
-// const nameLenghts = users.map(function(name){
-//     return name.length;
-// });
-
-// console.log(nameLenghts) // (3) [6, 3, 4]
-
-//Shorter
-// const nameLenghts = users.map((name) =>{
-//     return name.length;
-// });
-
-//Shortest
-const nameLenghts = users.map(name => name.length);
-
-console.log(nameLenghts)
+    // Get from external url
+    function getUrl(){
+        fetch('https://api.github.com/users')
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);//   document.querySelector(".output").innerHTML= res; // it will print object object on the screen not the actual output becoz in json array is threr
+             let output = '';
+             data.forEach(function(user){
+                 output += `<li>${user.login}</li>`;
+    
+             });
+             document.querySelector(".output").innerHTML= output; // it will print on screen
+            })
+        .catch(err => console.log(err))
+        }
